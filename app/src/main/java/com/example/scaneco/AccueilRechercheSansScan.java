961f1f9a6f6314ccacf55536560a363799246f4a
Nunez Dorian <doriannunez64@gmail.com>
2022-01-27 16:12:20 +0100

    Ajout de la barre de navigation ainsi que le bouton retour scan dans le XML

--- app/src/main/java/com/example/scaneco/AccueilRechercheSansScan.java
+++ app/src/main/java/com/example/scaneco/AccueilRechercheSansScan.java
@@ -8,23 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class AccueilRechercheSansScan extends AppCompatActivity {
-    private Button _boutonRetourScan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_recherche_sans_scan);
-        _boutonRetourScan = (Button) findViewById(R.id.boutonRetourScan);
-        _boutonRetourScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ouvrirLeScanneur();
-            }
-        });
-    }
 
-    /*Fonction qui permet d'accéder au scanneur */
-    public void ouvrirLeScanneur(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
