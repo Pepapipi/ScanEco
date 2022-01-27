7a70363aa21554f6b03f13928d57f4c57e594724
Nunez Dorian <doriannunez64@gmail.com>
2022-01-27 18:08:47 +0100

    Ajout de la barre de navigation ainsi que le bouton retour scan dans le XML

--- app/src/main/java/com/example/scaneco/AccueilRechercheSansScan.java
+++ app/src/main/java/com/example/scaneco/AccueilRechercheSansScan.java
@@ -6,13 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AccueilRechercheSansScan extends AppCompatActivity {
 
+    private ImageButton _boutonRetourScan;
+    private ScrollView _scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_recherche_sans_scan);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case R.id.accueilHorRamPoubelles:
+                    ouvrirHorRamPoubelles();
+                    break;
+
+            }
+            return true;
+        });
+
+        _boutonRetourScan = findViewById(R.id.boutonRetourScan);
+        _boutonRetourScan.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                ouvrirLeScan();
+            }
+        });
+    }
+
+    public void ouvrirLeScan()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void ouvrirHorRamPoubelles()
+    {
+        Intent intent = new Intent(this, AccueilHorRamPoubelles.class);
+        startActivity(intent);
     }
 }
