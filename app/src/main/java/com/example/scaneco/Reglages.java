394f67c0b1fc2fad139c10de2a2a54c5dfe7e235
Antoine <antoine.graire@gmail.com>
2022-01-29 13:28:59 +0100

    Sauvegarde dans les réglages du mode sombre ou clair

--- app/src/main/java/com/example/scaneco/Reglages.java
+++ app/src/main/java/com/example/scaneco/Reglages.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -12,6 +14,8 @@ import android.widget.ImageButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 public class Reglages extends AppCompatActivity {
 
@@ -27,6 +31,16 @@ public class Reglages extends AppCompatActivity {
         _switch = findViewById(R.id.switchSombreClair);
         _boutonFlecheRetourScan = findViewById(R.id.boutonFlecheRetourScan);
 
+
+        //Enregistrer le status de switch
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences.Editor editor = preferences.edit();
+        if (preferences.getBoolean("checked", false) == true){
+            _switch.setChecked(true);
+        } else {
+            _switch.setChecked(false);
+        }
+
         //Lorsqu'on clique sur la flèche de retour, on revient sur la page de scan
         _boutonFlecheRetourScan.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v)
@@ -40,10 +54,16 @@ public class Reglages extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 //Si le switch est activé alors on active le mode sombre
                 if (isChecked) {
+                    //On met le checked en true pour dire que le switch est activé
+                    editor.putBoolean("checked", true);
+                    editor.apply();
                     //Permet de mettre le mode sombre
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 //Sinon on active pas le mode sombre (on reste sur le mode clair)
                 } else {
+                    //On met le checked en true pour dire que le switch est activé
+                    editor.putBoolean("checked", false);
+                    editor.apply();
                     //Permet de ne pas mettre le mode sombre
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
