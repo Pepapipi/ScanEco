fa9c92478dd3e12e6106dea1fb9c01e79dfb1e1d
Antoine <antoine.graire@gmail.com>
2022-02-15 13:45:38 +0100

    Création page "Points de collecte"

---
+++ app/src/main/java/com/example/scaneco/PointsDeCollecte.java
@@ -0,0 +1,14 @@
+package com.example.scaneco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PointsDeCollecte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points_de_collecte);
+    }
+}
