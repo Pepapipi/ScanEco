a69f95e66486a6feeff92b5803cf9e25fb7d3c95
Nunez Dorian <doriannunez64@gmail.com>
2022-02-15 02:39:52 +0100

    Gestion du clavier sur la recherche sans scan

--- app/src/main/java/com/example/scaneco/recherchesansscan/AccueilRechercheSansScan.java
+++ app/src/main/java/com/example/scaneco/recherchesansscan/AccueilRechercheSansScan.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.*;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +17,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -50,10 +54,13 @@ public class AccueilRechercheSansScan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Empêche le clavier de se réactiver quand on revient en arrière après avoir vu un produit
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_recherche_sans_scan);
-
-        _gridLayout = (GridLayout)findViewById(R.id.gridViewRechercheSansScan);
+        _gridLayout = (GridLayout) findViewById(R.id.gridViewRechercheSansScan);
         setSingleEvent(_gridLayout);
         //Barre de navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -83,34 +90,33 @@ public class AccueilRechercheSansScan extends AppCompatActivity {
 
     }
 
-    protected void ouvrirLeScan()
-    {
+    protected void ouvrirLeScan() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    protected void ouvrirHorRamPoubelles()
-    {
+
+    protected void ouvrirHorRamPoubelles() {
         Intent intent = new Intent(this, AccueilHorRamPoubelles.class);
         startActivity(intent);
     }
-    public void ouvrirAnimations()
-    {
+
+    public void ouvrirAnimations() {
         Intent intent = new Intent(this, AccueilAnimations.class);
         startActivity(intent);
     }
 
-    protected void setSingleEvent (GridLayout mainGrid){
+    protected void setSingleEvent(GridLayout mainGrid) {
         arrayEmballage.addAll(Arrays.asList(getResources().getStringArray(R.array.mes_dechets)));
 
-        for (int i = 0; i< mainGrid.getChildCount(); i++){
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
             CardView _cardView = (CardView) mainGrid.getChildAt(i);
             final int _finalI = i;
             _cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                Intent intent = new Intent(AccueilRechercheSansScan.this, RechercheSansScanCategDetails.class);
-                intent.putExtra("info", arrayEmballage.get(_finalI));
-                startActivity(intent);
+                    Intent intent = new Intent(AccueilRechercheSansScan.this, RechercheSansScanCategDetails.class);
+                    intent.putExtra("info", arrayEmballage.get(_finalI));
+                    startActivity(intent);
                 }
             });
         }
@@ -119,51 +125,45 @@ public class AccueilRechercheSansScan extends AppCompatActivity {
 
     @SuppressLint("NotifyDataSetChanged")
     private void initRecyclerView() {
-
         scrlView = findViewById(R.id.scrollView);
         scrlView.setVisibility(View.INVISIBLE);
         recyclerView = findViewById(R.id.recy);
         layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new ProgramAdapter(produits,recyclerViewClickListner);
+        adapter = new ProgramAdapter(produits, recyclerViewClickListner);
         recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
     }
 
-    private void rechercheDuProduit(String s)
-    {
+    private void rechercheDuProduit(String s) {
         jsonFromKeyword = new JsonFromKeyword();
-        jsonFromKeyword.activity=this;
+        jsonFromKeyword.activity = this;
         jsonFromKeyword.execute(s);
     }
 
-    public void jsonGot(String json){
+    public void jsonGot(String json) {
         TextView txt = findViewById(R.id.text);
         try {
             produits = Produit.getProductsListFromJson(json);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             txt.setText(e.toString());
         }
         initRecyclerView();
     }
 
     /**
-     *
      * @param menu
      * @return boolean
      * Quand l'utilisateur clique sur la loupe et écrit le nom du produit
      * Il faut voir si c'est un code-barres
      */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_recherche_sans_scan,menu);
+        inflater.inflate(R.menu.menu_recherche_sans_scan, menu);
         MenuItem item = menu.findItem(R.id.search_product);
-        SearchView searchView = (SearchView)item.getActionView();
-
+        SearchView searchView = (SearchView) item.getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -173,27 +173,27 @@ public class AccueilRechercheSansScan extends AppCompatActivity {
                  */
                 try {
                     long nombreRecup = Long.parseLong(s);
-                    if (s.length()==8||s.length()==13)
-                    {
+                    if (s.length() == 8 || s.length() == 13) {
                         //Il faut maintenant envoyé les données à la page produit.
                         Produit _produitAEnvoyer = Produit.getProductFromBarCode(s);
                         Intent intent = new Intent(getApplicationContext(), ProduitDetails.class);
                         intent.putExtra("codeBarre", _produitAEnvoyer.getCode());
                         startActivity(intent);
-                        Toast.makeText(AccueilRechercheSansScan.this,"Yes codeBrre Ok", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(AccueilRechercheSansScan.this, "Yes codeBrre Ok", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(AccueilRechercheSansScan.this, "Malheuresement le code barre est faux", Toast.LENGTH_SHORT).show();
                     }
-                    else
-                    {
-                        Toast.makeText(AccueilRechercheSansScan.this,"Malheuresement le code barre est faux", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                catch (Exception e)
-                {
-                    Toast.makeText(AccueilRechercheSansScan.this,"On lance la recherche", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(AccueilRechercheSansScan.this, "On lance la recherche", Toast.LENGTH_SHORT).show();
                     rechercheDuProduit(s);
                 }
+
+                //Cache le clavier après que l'utilisateur ait validé sa saisie
+                hideKeyboard(AccueilRechercheSansScan.this);
+
                 return true;
             }
+
             @Override
             public boolean onQueryTextChange(String s) {
                 return false;
@@ -212,7 +212,7 @@ public class AccueilRechercheSansScan extends AppCompatActivity {
      * Il aura un petit message d'erreur pour le moment, ensuite on le redirigera possiblement sur OFF
      * sur le produit qui n'a pas d'emballage
      */
-    private void setOnClickListner(){
+    private void setOnClickListner() {
         recyclerViewClickListner = new RecyclerViewClickListner() {
             @Override
             public void onClick(View v, int position) {
@@ -220,12 +220,22 @@ public class AccueilRechercheSansScan extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), ProduitDetails.class);
                     intent.putExtra("codeBarre", produits.get(position).getCode());
                     startActivity(intent);
-                }
-                catch (Exception e) {
-                    Toast.makeText(AccueilRechercheSansScan.this,e.toString(),Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(AccueilRechercheSansScan.this, e.toString(), Toast.LENGTH_SHORT).show();
                 }
             }
 
         };
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
