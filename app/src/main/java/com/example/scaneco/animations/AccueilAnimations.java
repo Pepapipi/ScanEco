5ff2ba7da7870e3d95e5ff0b9538abede63cd7d7
Nunez Dorian <doriannunez64@gmail.com>
2022-02-19 15:00:12 +0100

    Redirection sur youtube quand on clique sur une vidéo

--- app/src/main/java/com/example/scaneco/animations/AccueilAnimations.java
+++ app/src/main/java/com/example/scaneco/animations/AccueilAnimations.java
@@ -1,13 +1,17 @@
 package com.example.scaneco.animations;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -16,20 +20,17 @@ import com.example.scaneco.R;
 import com.example.scaneco.horRamPoubelles.AccueilHorRamPoubelles;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.InputStream;
+import java.net.URL;
+
 
 public class AccueilAnimations extends AppCompatActivity {
-
-
     private ImageButton _boutonRetourScan;
     private VideoView _videoView;
-    private VideoView _video2;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_animations);
-
         //BARRE DE NAVIGATION
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -37,39 +38,76 @@ public class AccueilAnimations extends AppCompatActivity {
                 case R.id.accueilHorRamPoubelles:
                     ouvrirHorRamPoubelles();
                     break;
-
             }
             return true;
         });
         //Bouton de retour
         _boutonRetourScan = findViewById(R.id.boutonRetourScan);
-        _boutonRetourScan.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ouvrirLeScan();
-            }
-        });
+        _boutonRetourScan.setOnClickListener(v -> ouvrirLeScan());
 
 
 
         //VIDEOS
 
-        _videoView = findViewById(R.id.videoView);
-        String videoPath = "android.resource://"+getPackageName()+"/"+R.raw.amogus;
-        Uri uri = Uri.parse(videoPath);
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(_videoView);
-        _videoView.setMediaController(mediaController);
-        _videoView.setVideoURI(uri);
+        String videoKey = "REh-GAV1cfA";
+        ImageView imageView = findViewById(R.id.imageView1);
+        imageView.setOnClickListener(v -> {
+            Log.println(Log.INFO, "info", "redirection");
+            Intent youtube = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + videoKey));
+            youtube.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            youtube.setPackage("com.google.android.youtube");
+            try {
+                getApplication().startActivity(youtube);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        try {
+            new SetImageFromUrl(imageView, "https://img.youtube.com/vi/" + videoKey + "/0.jpg");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-        _video2 = findViewById(R.id.videoView2);
-        String videoPath2 = "android.resource://"+getPackageName()+"/"+R.raw.widescaneco;
-        Uri uri2 = Uri.parse(videoPath2);
-        MediaController mediaController2 = new MediaController(this);
-        mediaController2.setAnchorView(_video2);
-        _video2.setMediaController(mediaController2);
-        _video2.setVideoURI(uri2);
+        String videoKey2 = "MECmgIz36nU";
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        imageView2.setOnClickListener(v -> {
+            Log.println(Log.INFO, "info", "redirection");
+            Intent youtube2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + videoKey2));
+            youtube2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            youtube2.setPackage("com.google.android.youtube");
+            try {
+                getApplication().startActivity(youtube2);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        try {
+            new SetImageFromUrl(imageView2, "https://img.youtube.com/vi/" + videoKey2 + "/0.jpg");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-
+        String videoKey3 = "w6WTuAl18CA";
+        ImageView imageView3 = findViewById(R.id.imageView3);
+        imageView3.setOnClickListener(v -> {
+            Log.println(Log.INFO, "info", "redirection");
+            Intent youtube3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + videoKey3));
+            youtube3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            youtube3.setPackage("com.google.android.youtube");
+            try {
+                getApplication().startActivity(youtube3);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        try {
+            new SetImageFromUrl(imageView3, "https://img.youtube.com/vi/" + videoKey3 + "/0.jpg");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     protected void ouvrirLeScan()
@@ -81,7 +119,4 @@ public class AccueilAnimations extends AppCompatActivity {
     {
         Intent intent = new Intent(this, AccueilHorRamPoubelles.class);
         startActivity(intent);
-    }
-
-
-}
+    }}
