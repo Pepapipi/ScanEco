4fd6688e68fa37d25237c6ca8c7070624328665e
Luc Leydert <luc.leydert@gmail.com>
2022-03-26 13:06:06 +0100

    SonarLint sur HorRamPoubellesDetailsVille.java

--- app/src/main/java/com/example/scaneco/horrampoubelles/HorRamPoubellesDetailsVille.java
+++ app/src/main/java/com/example/scaneco/horrampoubelles/HorRamPoubellesDetailsVille.java
@@ -2,26 +2,14 @@ package com.example.scaneco.horrampoubelles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.scaneco.R;
 
-import java.util.ArrayList;
 
 public class HorRamPoubellesDetailsVille extends AppCompatActivity {
 
-    private ImageButton _boutonRetourScan;
-
-
-    ArrayAdapter<String> adapter;
-    ArrayList<String> arrayVille;
-    ListView recherche_ville;
 
     static Ville villeRecuperee;
 
@@ -32,15 +20,12 @@ public class HorRamPoubellesDetailsVille extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hor_ram_poubelles_details_ville);
 
-        TextView textViewJoursSelectifs = (TextView) findViewById(R.id.joursSelectifs);
-        TextView textViewJoursMenagers = (TextView) findViewById(R.id.joursMenagers);
-        TextView textViewHeuresSelectifs = (TextView) findViewById(R.id.heuresSelectifs);
-        TextView textViewHeuresMenagers = (TextView) findViewById(R.id.heuresMenagers);
+        TextView textViewJoursSelectifs = findViewById(R.id.joursSelectifs);
+        TextView textViewJoursMenagers = findViewById(R.id.joursMenagers);
+        TextView textViewHeuresSelectifs = findViewById(R.id.heuresSelectifs);
+        TextView textViewHeuresMenagers = findViewById(R.id.heuresMenagers);
 
         TextView textView;
-        ImageView imageView;
-        Bitmap bImage;
-
 
         textView = findViewById(R.id.NomDeLaVille);
 
@@ -54,16 +39,12 @@ public class HorRamPoubellesDetailsVille extends AppCompatActivity {
                 villeRecuperee.codePostal.equals("64100")||
                 villeRecuperee.codePostal.equals("64340")||villeRecuperee.codePostal.equals("64210"))
         {
-            //bImage= BitmapFactory.decodeResource(this.getResources(), R.drawable.cartographie_bayonne);
-            imageView = findViewById(R.id.cartographie);
-            //imageView.setImageBitmap(bImage);
+            findViewById(R.id.cartographie);
         }
 
         if (villeRecuperee.codePostal.equals("64990"))
         {
-            //bImage = BitmapFactory.decodeResource(this.getResources(), R.drawable.saint_pierre_dirube);
-            imageView = findViewById(R.id.cartographie);
-            //imageView.setImageBitmap(bImage);
+            findViewById(R.id.cartographie);
         }
 
 
@@ -75,115 +56,3 @@ public class HorRamPoubellesDetailsVille extends AppCompatActivity {
 
     }
 }
-
-
-
-
-
-
-
-/*
-
-    Bundle b = getIntent().getExtras();
-    //VilleList maliste = getIntent().getExtras().get("nomDeLaListe");
-    Ville ville= (Ville) getIntent().getExtras().get("nomDeLaListe");
-
-        for (int i = 0; i < maliste.size(); i++) {
-            arrayVille.add(maliste.get(i).nom);
-        }
-
-
-    recherche_ville = (ListView) findViewById(R.id.recherche_ville);
-
-
-    adapter = new ArrayAdapter<String>(
-    HorRamPoubellesDetailsVille.this,
-    android.R.layout.simple_list_item_1,
-    arrayVille
-        );
-        recherche_ville.setAdapter(adapter);
-
-        recherche_ville.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            Toast.makeText(HorRamPoubellesDetailsVille.this, "Selectionné " + recherche_ville.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-            //villeSelectionnee=new Ville();
-            //villeSelectionnee=listVille.get(position).get(0);
-
-            Intent intent;
-            //intent.putExtra("nombreDeVilleDansLaListe",nombreDeVille);
-
-            String nomDeLaListe = recherche_ville.getItemAtPosition(position).toString();
-
-        }
-    });
-
-}
-
-
-    ///////////////Loupe de recherche\\\\\\\\\\\\\\\
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_hor_ram_poubelles, menu);
-        MenuItem item = menu.findItem(R.id.search_ville);
-        SearchView searchView = (SearchView) item.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                adapter.getFilter().filter(s);
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }
-        /////////////////Navigation\\\\\\\\\\\\\\\\\\\\
-
-    /*
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.accueilHorRamPoubelles:
-                    ouvrirHorRamPoubelles();
-                    break;
-
-            }
-            return true;
-        });
-
-        _boutonRetourScan = findViewById(R.id.boutonRetourScan);
-        _boutonRetourScan.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ouvrirLeScan();
-            }
-        });
-    }
-
-
-
-
-
-
-
-
-    public void ouvrirLeScan()
-    {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-    public void ouvrirHorRamPoubelles()
-    {
-        Intent intent = new Intent(this, AccueilHorRamPoubelles.class);
-        startActivity(intent);
-    }*/
-
-
