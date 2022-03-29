460e8f9ef815050ba6b12bcaf66c4665a2517f05
Nunez Dorian <doriannunez64@gmail.com>
2022-03-29 15:01:32 +0200

    Ajout d'un switch et d'une fonction ajouterBouton

--- app/src/androidTest/java/com/example/scaneco/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/scaneco/ExampleInstrumentedTest.java
@@ -23,4 +23,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.scaneco", appContext.getPackageName());
     }
+
 }
