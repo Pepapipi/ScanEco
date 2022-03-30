5bcbe8d1d41c111f7a7b3250462c9f0a10a301ce
Jules <jmarchepro@gmail.com>
2022-03-30 11:32:20 +0200

    Correction de la navigation

--- app/src/main/java/com/example/scaneco/horrampoubelles/HorRamPoubellesDetailsVille.java
+++ app/src/main/java/com/example/scaneco/horrampoubelles/HorRamPoubellesDetailsVille.java
@@ -2,10 +2,16 @@ package com.example.scaneco.horrampoubelles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.scaneco.MainActivity;
 import com.example.scaneco.R;
+import com.example.scaneco.animations.AccueilAnimations;
+import com.example.scaneco.pointdecollecte.RecherchePointDeCollecte;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class HorRamPoubellesDetailsVille extends AppCompatActivity {
@@ -54,5 +60,59 @@ public class HorRamPoubellesDetailsVille extends AppCompatActivity {
         textViewHeuresMenagers.setText(villeRecuperee.getHeuresMenagers());
 
 
+        //Initialisation de la barre de menu
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item ->{
+            int itemId = item.getItemId();
+            if (itemId == R.id.accueilHorRamPoubelles) {
+                ouvrirHorRamPoubelles();
+            } else if (itemId == R.id.accueilAnimations) {
+                ouvrirAnimations();
+            } else if (itemId == R.id.accueilPointDeCollecte) {
+                ouvrirRecherchePointDeCollecte();
+            }
+            return true;
+        });
+
+        //Bouton de retour
+        ImageButton boutonRetourScan = findViewById(R.id.boutonRetourScan);
+        boutonRetourScan.setOnClickListener(v -> ouvrirLeScan());    }
+
+    /////////////////Navigation\\\\\\\\\\\\\\\\\\\\
+
+    /**
+     * Fonction permettant d'aller sur une autre page(vue)
+     * Ici cette fonction permettra d'accéder aux Animations
+     **/
+    public void ouvrirAnimations()
+    {
+        Intent intent = new Intent(this, AccueilAnimations.class);
+        startActivity(intent);
     }
+
+    /**
+     * Fonction permettant d'aller sur une autre page(vue)
+     * Ici cette fonction permettra d'accéder à la page des horaires de ramassage des poubelles
+     **/
+    public void ouvrirHorRamPoubelles()
+    {
+        Intent intent = new Intent(this, AccueilHorRamPoubelles.class);
+        startActivity(intent);
+    }
+    /**
+     * Fonction permettant d'aller sur une autre page(vue)
+     * Ici cette fonction permettra d'accéder à la page des points de collecte
+     **/
+    protected void ouvrirRecherchePointDeCollecte()
+    {
+        Intent intent = new Intent(this, RecherchePointDeCollecte.class);
+        startActivity(intent);
+    }
+
+    protected void ouvrirLeScan()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
